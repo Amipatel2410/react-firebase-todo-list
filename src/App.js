a8d1694081d6b81f15a1162db6d25727163dd846
amisha patel <ami.patel2410@gmail.com>
2017-09-11 17:07:21 -0400

    viewing todos

--- src/App.js
+++ src/App.js
@@ -14,6 +14,22 @@ class App extends Component {
       };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.selectTodo = this.selectTodo.bind(this);
+    }
+
+    componentDidMount(){
+      axios({
+        url: '/todos.json',
+        baseURL: 'https://todo-list-app-6de68.firebaseio.com/',
+        method:'get',
+      })
+      .then((response)  => {
+        console.log(response);
+        this.setState({todos: response.data});
+      }).catch((error) => {
+        console.log(error);
+      });
     }
 
     handleSubmit(event) {
@@ -49,6 +65,45 @@ class App extends Component {
 
     }
 
+    deleteTodo(todoId)
+    {
+        axios({
+            url: `/todos/${todoId}.json`,
+            baseURL: 'https://todo-list-app-6de68.firebaseio.com/',
+            method: 'DELETE'
+          })
+        .then((response) => {
+            console.log(response);
+            let todos = this.state.todos;
+            delete todos[todoId];
+            this.setState({todos: todos});
+       })
+         .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    selectTodo(todoId)
+    {
+        this.setState({currentTodo: todoId});
+    }
+
+    renderSelectedTodo(){
+
+      let content;
+
+      if(this.state.currentTodo){
+      let currentTodo = this.state.todos[this.state.currentTodo];
+      content = (
+          <div>
+            <h2> {currentTodo.title} </h2>
+          </div>
+        );
+        }
+
+        return content;
+    }
+
     renderNewTodoBox() {
       return(
         <div className="new-todo-box pb-2">
@@ -71,10 +126,14 @@ class App extends Component {
 
           todoElements.push(
             <div className="todo d-flex justify-content-between pb-4" key={todoId}>
-            <div className="mt-2">
+            <div className="mt-2" onClick={() => this.selectTodo(todoId)}>
             <h4>{todo.title}</h4>
             <div>{moment(todo.createdAt).calendar()}</div>
               </div>
+            <button className="ml-4 btn btn-link"
+            onClick={() => {this.deleteTodo(todoId) }}>
+              <span aria-hidden="true">&times;</span>
+              </button>
             </div>
           );
          }
@@ -94,6 +153,9 @@ class App extends Component {
           <div className="col-6 px-4">
             {this.renderNewTodoBox()}
             {this.renderTodoList()}
+          </div>
+          <div className="col-6 px-4">
+            {this.renderSelectedTodo()}
           </div>
         </div>
       </div>
